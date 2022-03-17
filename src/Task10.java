3174cea963baf7eb41f1ad4103416c6949aaf908
temkame <temkame@yandex.ru>
2022-03-18 02:37:39 +0300

    Update

---
+++ src/Task10.java
@@ -0,0 +1,11 @@
+public class Task10 {
+    public static void print(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        print("Я тебя люблю");
+    }
+}
