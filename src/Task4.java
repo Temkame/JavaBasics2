ded4614c542dff7accc2b07f62a916537d3b5fbb
temkame <temkame@yandex.ru>
2022-03-18 01:29:23 +0300

    Task4

---
+++ src/Task4.java
@@ -0,0 +1,19 @@
+/*
+Создайте 10 переменных типа Cat и 8 объектов типа Cat.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+    Cat cat1 = new Cat();
+    Cat cat2 = new Cat();
+    Cat cat3 = new Cat();
+    Cat cat4 = new Cat();
+    Cat cat5 = new Cat();
+    Cat cat7 = new Cat();
+    Cat cat8 = new Cat();
+    Cat cat9;
+    Cat cat10;
+    }
+}
+class Cat {
+
+}
